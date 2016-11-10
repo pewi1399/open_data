35f1d8d8527b27eb21805ce288be2904ad641467
Per Wikman <Per.Wikman@pensionsmyndigheten.se>
2016-11-10 14:31:53 +0100

    update to dcat generator

--- dcat_generator.R
+++ dcat_generator.R
@@ -1,30 +1,11 @@
 library(whisker)
 library(magrittr)
+library(dplyr)
+library(tidyr)
+library(openxlsx)
 
 
-template <- 
-readLines("sandbox_min_template.xml") %>% 
-  paste(collapse = "\n")
-  
-PM_katalog <- "Pensionsmyndighetens datakatalog"
-Katalogbeskrivning <- "Katalogen innehåller information om det svenska pensionssystemet, tillhandahållna av Pensionsmyndigheten"
-Organisationsnamn <- "Pensionsmyndigheten"
-Datasetnamn <- "Utbetalningar bostadstillägg dec 2015"
-Dataset_beskrivning <- "Utbetalade belopp under decembermånad 2015. Geografisk koppling till kommun tillhandahålls i övrigt är data helt avidentifierat"
-
-out <- whisker.render(template)
+source("functions.R")
 
 
-
-meta_xlsx <- data.frame(
-  PM_katalog = "Pensionsmyndighetens datakatalog",
-  Katalogbeskrivning = "Katalogen innehåller information om det svenska pensionssystemet, tillhandahållna av Pensionsmyndigheten",
-  Organisationsnamn = "Pensionsmyndigheten",
-  Datasetnamn  = "Utbetalningar bostadstillägg dec 2015",
-  Dataset_beskrivning = "Utbetalade belopp under decembermånad 2015. Geografisk koppling till kommun tillhandahålls i övrigt är data helt avidentifierat"
-)
-
-out <- whisker.render(template, meta_xlsx)
-
-writeLines(out, "output.xml")
-
+generate_dcatxml("metadata.xlsx", "test1")
